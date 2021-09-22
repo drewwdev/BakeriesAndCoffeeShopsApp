796eafe3e79121ce149366d6ffce10237827bea3
Drew Wilson <jankyy93@gmail.com>
2021-09-22 17:54:25 -0400

    Migrations created

---
+++ Migrations/20210922215312_CreateBakeryAndCoffeeShop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BakeriesAndCoffeeShopsApp.Migrations
+{
+    public partial class CreateBakeryAndCoffeeShop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BakeriesAndCoffeeShops",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Bakery = table.Column<bool>(type: "boolean", nullable: false),
+                    CoffeeShop = table.Column<bool>(type: "boolean", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    MainImage = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BakeriesAndCoffeeShops", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BakeriesAndCoffeeShops");
+        }
+    }
+}
